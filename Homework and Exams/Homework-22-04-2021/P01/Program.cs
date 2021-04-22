06df0bc1300c2acbc511ed6589510b030203e42d
Kristiyan Ivanov Garchev <49957699+Kristyan025@users.noreply.github.com>
2021-04-22 23:23:10 +0300

    Adding homework from 22.04.2021

---
+++ Homework and Exams/Homework-22-04-2021/P01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace P01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int[,] arr = new int[n, m];
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < m; j++)
+                {
+                    arr[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            int mini;
+            for (int i = 0; i < n; i++)
+            {
+                mini = int.MaxValue;
+                for(int j = 0; j < m; j++)
+                {
+                    mini = arr[i, j] < mini ? arr[i, j] : mini;
+                }
+
+                Console.WriteLine(mini);
+            }
+        }
+    }
+}
---
+++ Homework and Exams/Homework-22-04-2021/P02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace P02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int[,] arr = new int[n, m];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    arr[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            int count;
+            for (int j = 0; j < m; j++)
+            {
+                count = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    if ((arr[i, j] & 1) == 0) count++;
+                }
+
+                Console.WriteLine(count);
+            }
+        }
+    }
+}
